1c10a7a544c634fb4cc4f72021f084b4d8b3a66a
jaridwarren <jaridwarren@gmail.com>
2018-08-02 15:47:25 -0700

    built out board, game, and paddle classes

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,62 @@
+import { SVG_NS, KEYS } from '../settings';
+import Board from './Board'; //added to instantiate board class
+import Paddle from './Paddle';
+
 export default class Game {
 
 	constructor(element, width, height) {
 		this.element = element;
 		this.width = width;
 		this.height = height;
+		this.gameElement = document.getElementById(this.element);
 	
-		// Other code goes here...
+		// instantiate a new board when we construct a game
+		this.board = new Board(this.width, this.height);
+		
+		// setting up player values
+		this.paddleWidth = 8;
+		this.paddleHeight = 56;
+		this.boardGap = 10;
+
+		// instantiate player1
+		this.player1 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			this.boardGap,
+			((this.height - this.paddleHeight) / 2),
+			KEYS.a,
+			KEYS.z
+		);
+
+		// instantiate player2
+		this.player2 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			(this.width - this.boardGap - this.paddleWidth),
+			((this.height - this.paddleHeight) / 2),
+			KEYS.up,
+			KEYS.down
+		);
 	}
 
 	render() {
-		// More code goes here...
+		// clear out existing elements
+		this.gameElement.innerHTML = '';
+		// create new elements
+		let svg = document.createElementNS(SVG_NS, 'svg');
+		svg.setAttributeNS(null, 'width', this.width);
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+		this.gameElement.appendChild(svg);
+
+		// render the board
+		this.board.render(svg);
+
+		// render the paddles
+		this.player1.render(svg);
+		this.player2.render(svg);
 	}
 
 }
---
+++ src/settings.js
@@ -0,0 +1,8 @@
+export const SVG_NS = "http://www.w3.org/2000/svg";
+export const KEYS = {
+  a: 'a',        // player 1 up key
+  z: 'z',        // player 1 down key
+  up: 'ArrowUp',       // player 2 up key
+  down: 'ArrowDown',     // player 2 down key
+  spaceBar: 'Space', // we'll use this later...
+}
