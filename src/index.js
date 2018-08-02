9895b4adf07c89230dcd067eb47c7ba1b887dbcf
jaridwarren <jaridwarren@gmail.com>
2018-08-02 10:44:27 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles/game.css';
+import Game from './partials/Game'
+
+// create a game instance
+const game = new Game('game', 512, 256);
+
+(function gameLoop() {
+    game.render();
+    requestAnimationFrame(gameLoop);
+})();
